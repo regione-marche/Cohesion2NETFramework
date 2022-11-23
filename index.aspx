3d1fef2099b9462516e279a942e870151dc57a70
Saverio Delpriori <saverio.delpriori@regione.marche.it>
2022-11-23 22:19:04 +0100

    Initial import

---
+++ index.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="TestCohesion.index" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+<table align="center">
+        <tr>
+        <td colspan="3">
+        <br />
+        <br />
+        <br />
+
+        </td>
+        </tr>
+            <tr>
+                <td colspan="3">
+                    <font face="Script MT Bold" color="339933" size="40pt"> <b>Test Cohesion 2.0 </b></font>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+               
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td align="center">
+                    <button type="button" id="Button1" onclick="self.location = './privato/pagina_protetta.aspx';">Accedi</button>
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+               
+                <br />
+                <br />
+
+                </td>
+            </tr>
+        </table>
+
+</body>
+</html>
---
+++ privato/pagina_protetta.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Questo codice è stato generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace TestCohesion.privato
+{
+
+
+    public partial class pagina_protetta
+    {
+
+        /// <summary>
+        /// Controllo form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controllo fcLbl.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label fcLbl;
+
+        /// <summary>
+        /// Controllo nomeLbl.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label nomeLbl;
+
+        /// <summary>
+        /// Controllo authLbl.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label authLbl;
+
+        /// <summary>
+        /// Controllo doctxb.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox doctxb;
+
+        /// <summary>
+        /// Controllo authtxb.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox authtxb;
+
+        /// <summary>
+        /// Controllo buttlogout.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button buttlogout;
+    }
+}
